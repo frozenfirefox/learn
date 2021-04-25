a2f441837af732d4cc7b1dd5026b1f01c1706fc8
dhw <dhw>
2021-04-25 15:06:42 +0800

    add portal

--- app/portal/controller/IndexController.php
+++ app/portal/controller/IndexController.php
@@ -14,8 +14,71 @@ use cmf\controller\HomeBaseController;
 
 class IndexController extends HomeBaseController
 {
+
     public function index()
     {
+        $this->assign("nav", "");
+        $this->assign("title", "广场-九九教育");
         return $this->fetch(':index');
+    }
+
+    /**
+     * 体验课
+     */
+    public function trial(){
+        $this->assign("nav", "trial");
+        $this->assign("title", "体验课-九九教育");
+        return $this->fetch(':trial');
+    }
+
+    /**
+     * 正式课
+     */
+    public function explore(){
+        $this->assign("nav", "explore");
+        $this->assign("title", "正式课-九九教育");
+        return $this->fetch(':explore');
+    }
+
+    /**
+     * activities
+     */
+    public function activities(){
+        $this->assign("nav", "activities");
+        $this->assign("title", "活动-九九教育");
+        return $this->fetch(':activities');
+    }
+
+    /**
+     * detail
+     */
+    public function detail(){
+        return $this->fetch(':detail');
+    }
+
+    /**
+     * courseDetail
+     */
+    public function courseDetail(){
+        $this->assign("nav", "");
+        $this->assign("title", "详情-九九教育");
+        return $this->fetch(':course_detail');
+    }
+
+    /**
+     * actDetail
+     */
+    public function actDetail(){
+        return $this->fetch(':act_detail');
+    }
+
+    public function block()
+    {
+        return $this->fetch();
+    }
+
+    public function ws()
+    {
+        return $this->fetch(':ws');
     }
 }
